d1a7fe2a0bc432458a867b9b1e9a451095917cf1
Cade Heck <cadeheck16@gmail.com>
2022-07-27 14:09:13 -0400

    .

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&jbf4tyar&!)o7&!^t&dcb7&!*(f%-vsk4=t1lsz-fg%y_12il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['red-sporting.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['red-sporting.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
